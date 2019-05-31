0876e25c8b9523deb64d95de166cf759c8b1d5e2
Max Lapan <max.lapan@gmail.com>
2019-05-31 07:21:27 +0300

    Fix drawing

--- firmware/badge1.X/src/xonix.c
+++ firmware/badge1.X/src/xonix.c
@@ -18,6 +18,7 @@ void xonix_main() {
         }
         
         // read input
+        xonix_draw_field();
     }
 }
 
@@ -46,7 +47,44 @@ void xonix_init_field() {
 
 
 void xonix_draw_field() {
+    uint8_t cur_bit = 0, start = 0, end = 0;
+    uint8_t mask, val, same, r, c;
+
     tft_fill_area(0, 0, TFT_WIDTH-1, TFT_HEIGHT-1, 0x000000);
+    
+    for (r = 0; r < FIELD_HEIGHT; r++) {
+        cur_bit = start = end = 0;
+        for (c = 0; c < FIELD_BYTES; c++) {
+            mask = 1 << 7;
+            val = field[r][c];
+            
+            while (mask) {
+                if (cur_bit)
+                    same = val & mask;
+                else
+                    same = ~val & mask;
+                if (same)
+                    end += 1;
+                else {
+                    tft_fill_area(r*CELL_SIZE, start*CELL_SIZE, 
+                            (end-start)*CELL_SIZE, CELL_SIZE, 
+                            cur_bit ? COLOR_GRASS : COLOR_ROAD);
+                    cur_bit = 1-cur_bit;
+                    start = end;
+                    end = start+1;
+                            
+                }
+                        
+                mask >>= 1;
+            }
+        }
+
+        if (end > start) {
+            tft_fill_area(r*CELL_SIZE, start*CELL_SIZE, 
+                    (end-start)*CELL_SIZE, CELL_SIZE, 
+                    cur_bit ? COLOR_GRASS : COLOR_ROAD);
+        }
+    }
 }
 
 
--- firmware/badge1.X/src/xonix.h
+++ firmware/badge1.X/src/xonix.h
@@ -1,12 +1,18 @@
 #ifndef __BADGE_H
 #define __BADGE_H
 
+#include <stdint.h>
 #include "disp.h"
 
 #define CELL_SIZE           3             // how large one item of the field
 #define FIELD_WIDTH         (TFT_WIDTH/CELL_SIZE)
 #define FIELD_HEIGHT        (TFT_HEIGHT/CELL_SIZE)
 #define FIELD_BYTES         ((FIELD_WIDTH+7)/8)
+
+#define COLOR_GRASS     0x00FF00
+#define COLOR_ROAD      0x0000F0
+#define COLOR_PLAYER    0x0000FF
+#define COLOR_ENEMY     0xFFFF00
 
 void xonix_main(void);
 void xonix_step(void);
