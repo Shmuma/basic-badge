a05c215f2e29bbad9b12715e07f6223b54c28d2b
Max Lapan <max.lapan@gmail.com>
2019-06-28 18:42:33 +0200

    Double the width of the screen

--- firmware/badge1.X/src/atari/atari_badge.c
+++ firmware/badge1.X/src/atari/atari_badge.c
@@ -12,14 +12,12 @@ void tia_line_ready(uint8_t line) {
     uint8_t x;
     uint32_t c;
 
-//    c = COLOR_NTSC(tia.fb[0]);
-//    tft_fill_area(0, tia.scanline, FB_WIDTH, 1, c);
-
-    tft_set_write_area(0, line, FB_WIDTH, 1);
+    tft_set_write_area(0, line, FB_WIDTH*2, 1);
     TFT_24_7789_Write_Command(0x2C);
     
     for (x = 0; x < FB_WIDTH; x++) {
         c = COLOR_NTSC(tia.fb[x]);
+        TFT_24_7789_Write_Data3((c >> 16) & 0xFF, (c >> 8) & 0xFF, c & 0xFF);
         TFT_24_7789_Write_Data3((c >> 16) & 0xFF, (c >> 8) & 0xFF, c & 0xFF);
     }
 }
